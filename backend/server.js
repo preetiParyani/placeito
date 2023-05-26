417fe3132013ebadd201a129e7d254124b4f57d5
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-05-26 23:18:04 +0530

    Created homeroute

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log('Hello Server')
+const express = require('express')
+const app = express()
+const homeRoute = require('./routes/home')
+const adminRoute = require('./routes/admin')
+
+
+//Set the view engine
+app.set('view engine', 'ejs')
+
+// Body parser
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Express automatically making route for static files in public directory
+app.use(express.static('public'));
+
+// Using the following routes
+app.use('/', homeRoute)
+app.use('/admin', adminRoute)
+
+app.listen(3000, ()=>{
+    console.log(`Server running on port 3000`)
+})
